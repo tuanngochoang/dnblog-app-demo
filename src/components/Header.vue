510adc3a834aefaba43e7d309de9301ff066aca3
naut74 <ngoctuan14032001@gmail.com>
2025-06-02 18:11:41 +0700

    create header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,276 @@
 <template>
-  <header class="header">
-    <h1>📝 My Vue Blog</h1>
-  </header>
+  <!--Offcanvas sidebar-->
+  <aside id="sidebar-wrapper" class="custom-scrollbar offcanvas-sidebar">
+        <button class="off-canvas-close"><i class="elegant-icon icon_close"></i></button>
+        <div class="sidebar-inner">
+            <!--Categories-->
+            <div class="sidebar-widget widget_categories mb-50 mt-30">
+                <div class="widget-header-2 position-relative">
+                    <h5 class="mt-5 mb-15">Hot topics</h5>
+                </div>
+                <div class="widget_nav_menu">
+                    <ul>
+                        <li class="cat-item cat-item-2"><a href="category.html">Travel tips</a> <span class="post-count">30</span></li>
+                        <li class="cat-item cat-item-3"><a href="category-grid.html">Book review</a> <span class="post-count">25</span></li>
+                        <li class="cat-item cat-item-4"><a href="category-list.html">Hotel review</a> <span class="post-count">16</span></li>
+                        <li class="cat-item cat-item-5"><a href="category-masonry.html">Destinations </a> <span class="post-count">22</span></li>
+                        <li class="cat-item cat-item-6"><a href="category-big.html">Lifestyle</a> <span class="post-count">25</span></li>
+                    </ul>
+                </div>
+            </div>
+            <!--Latest-->
+            <div class="sidebar-widget widget-latest-posts mb-50">
+                <div class="widget-header-2 position-relative mb-30">
+                    <h5 class="mt-5 mb-30">Don't miss</h5>
+                </div>
+                <div class="post-block-list post-module-1 post-module-5">
+                    <ul class="list-post">
+                        <li class="mb-30">
+                            <div class="d-flex hover-up-2 transition-normal">
+                                <div class="post-thumb post-thumb-80 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
+                                    <a class="color-white" href="single.html">
+                                        <img src=".../assets/imgs/news/thumb-1.jpg" alt="">
+                                    </a>
+                                </div>
+                                <div class="post-content media-body">
+                                    <h6 class="post-title mb-15 text-limit-2-row font-medium"><a href="single.html">The Life of a Travel Writer with David Farley</a></h6>
+                                    <div class="entry-meta meta-1 float-start font-x-small text-uppercase">
+                                        <span class="post-on">05 August</span>
+                                        <span class="post-by has-dot">300 views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mb-30">
+                            <div class="d-flex hover-up-2 transition-normal">
+                                <div class="post-thumb post-thumb-80 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
+                                    <a class="color-white" href="single.html">
+                                        <img src="../assets/imgs/news/thumb-2.jpg" alt="">
+                                    </a>
+                                </div>
+                                <div class="post-content media-body">
+                                    <h6 class="post-title mb-15 text-limit-2-row font-medium"><a href="single.html">Why Don’t More Black American Women Travel Solo?</a></h6>
+                                    <div class="entry-meta meta-1 float-start font-x-small text-uppercase">
+                                        <span class="post-on">12 August</span>
+                                        <span class="post-by has-dot">23k views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="mb-30">
+                            <div class="d-flex hover-up-2 transition-normal">
+                                <div class="post-thumb post-thumb-80 d-flex mr-15 border-radius-5 img-hover-scale overflow-hidden">
+                                    <a class="color-white" href="single.html">
+                                        <img src="../assets/imgs/news/thumb-3.jpg" alt="">
+                                    </a>
+                                </div>
+                                <div class="post-content media-body">
+                                    <h6 class="post-title mb-15 text-limit-2-row font-medium"><a href="single.html">The 22 Best Things to See and Do in Bangkok</a></h6>
+                                    <div class="entry-meta meta-1 float-start font-x-small text-uppercase">
+                                        <span class="post-on">27 August</span>
+                                        <span class="post-by has-dot">23k views</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--Ads-->
+            <div class="sidebar-widget widget-ads">
+                <div class="widget-header-2 position-relative mb-30">
+                    <h5 class="mt-5 mb-30">Advertise banner</h5>
+                </div>
+                <a href="https://themeforest.net/user/alithemes/portfolio" target="_blank">
+                    <img class="advertise-img border-radius-5" src="../assets/imgs/ads/ads-1.jpg" alt="">
+                </a>
+            </div>
+        </div>
+    </aside>
+    <!-- Start Header -->
+    <header class="main-header header-style-1 font-heading">
+        <div class="header-top">
+            <div class="container">
+                <div class="row pt-20 pb-20">
+                    <div class="col-md-3 col-xs-6">
+                        <a href="index.html"><img class="logo" src="../assets/imgs/theme/logo.png" alt=""></a>
+                    </div>
+                    <div class="col-md-9 col-xs-6 text-end header-top-right ">
+                        <ul class="list-inline nav-topbar d-none d-md-inline">
+                            <li class="list-inline-item menu-item-has-children"><a href="#">Layouts</a>
+                                <ul class="sub-menu font-small">
+                                    <li class="menu-item-has-children"><a href="#">Pages</a>
+                                        <ul class="sub-menu font-small">
+                                            <li><a href="page-about.html">About</a></li>
+                                            <li><a href="page-contact.html">Contact</a></li>
+                                            <li><a href="page-typography.html">Typography</a></li>
+                                            <li><a href="page-register.html">Register</a></li>
+                                            <li><a href="page-login.html">Login</a></li>
+                                            <li><a href="page-search.html">Search</a></li>
+                                            <li><a href="page-author.html">Author</a></li>
+                                            <li><a href="page-404.html">404 page</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="#">Category</a>
+                                        <ul class="sub-menu font-small">
+                                            <li><a href="category-list.html">List layout</a></li>
+                                            <li><a href="category-grid.html">Grid layout</a></li>
+                                            <li><a href="category-masonry.html">Masonry layout</a></li>
+                                            <li><a href="category-big.html">Big layout</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item-has-children"><a href="#">Single post</a>
+                                        <ul class="sub-menu font-small">
+                                            <li><a href="single.html">Default</a></li>
+                                            <li><a href="single-2.html">Big image</a></li>
+                                            <li><a href="single-3.html">Left image</a></li>
+                                            <li><a href="single-4.html">With sidebar</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="list-inline-item"><a href="http://demos.alithemes.com/html/stories/docs/"><i class="elegant-icon icon_document_alt mr-5"></i>Document</a></li>
+                        </ul>
+                        <span class="vertical-divider mr-20 ml-20 d-none d-md-inline"></span>
+                        <button class="search-icon d-none d-md-inline"><span class="mr-15 text-muted font-small"><i class="elegant-icon icon_search mr-5"></i>Search</span></button>
+                        <div class="dark-light-mode-cover">
+                            <a class="dark-light-mode" href="#"></a>
+                        </div>
+                        <a class="btn btn-radius bg-primary text-white ml-15 font-small box-shadow" href="https://themeforest.net/item/stories-personal-blog-html-template/28453702" target="_blank">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-sticky">
+            <div class="container align-self-center position-relative">
+                <div class="mobile_menu d-lg-none d-block"></div>
+                <div class="main-nav d-none d-lg-block float-start">
+                    <nav>
+                        <!--Desktop menu-->
+                        <ul class="main-menu d-none d-lg-inline font-small">
+                            <li class="menu-item-has-children">
+                                <a href="index.html"> <i class="elegant-icon icon_house_alt mr-5"></i> Home</a>
+                                <ul class="sub-menu text-muted font-small">
+                                    <li><a href="index.html">Home default</a></li>
+                                    <li><a href="home-2.html">Homepage 2</a></li>
+                                    <li><a href="home-3.html">Homepage 3</a></li>
+                                </ul>
+                            </li>
+                            <li> <a href="category-list.html">Travel</a> </li>
+                            <li class="current-item has-mega-menu">
+                                <a href="category-list.html">Mega Menu</a>
+                                <ul class="mega-menu">
+                                    <li class="sub-mega-menu sub-mega-menu-width-22">
+                                        <a class="menu-title" href="#">Travel Blog</a>
+                                        <ul>
+                                            <li><a href="category-list.html">Destinations</a></li>
+                                            <li><a href="category-list.html">Tour Guides</a></li>
+                                            <li><a href="category-list.html">Travel Food</a></li>
+                                            <li><a href="category-list.html">Hotels Booking</a></li>
+                                            <li><a href="category-list.html">Transport Review</a></li>
+                                            <li><a href="category-list.html">Travel Healthy</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="sub-mega-menu sub-mega-menu-width-22">
+                                        <a class="menu-title" href="#">Fruit &amp; Vegetables</a>
+                                        <ul>
+                                            <li><a href="category-list.html">Meat &amp; Poultry</a></li>
+                                            <li><a href="category-list.html">Fresh Vegetables</a></li>
+                                            <li><a href="category-list.html">Herbs &amp; Seasonings</a></li>
+                                            <li><a href="category-list.html">Cuts &amp; Sprouts</a></li>
+                                            <li><a href="category-list.html">Exotic Fruits &amp; Veggies</a></li>
+                                            <li><a href="category-list.html">Packaged Produce</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="sub-mega-menu sub-mega-menu-width-22">
+                                        <a class="menu-title" href="#">Breakfast &amp; Dairy</a>
+                                        <ul>
+                                            <li><a href="category-list.html">Milk &amp; Flavoured Milk</a></li>
+                                            <li><a href="category-list.html">Butter and Margarine</a></li>
+                                            <li><a href="category-list.html">Eggs Substitutes</a></li>
+                                            <li><a href="category-list.html">Marmalades</a></li>
+                                            <li><a href="category-list.html">Sour Cream</a></li>
+                                            <li><a href="category-list.html">Cheese</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="sub-mega-menu sub-mega-menu-width-22">
+                                        <a class="menu-title" href="#">Meat &amp; Seafood</a>
+                                        <ul>
+                                            <li><a href="category-list.html">Breakfast Sausage</a></li>
+                                            <li><a href="category-list.html">Dinner Sausage</a></li>
+                                            <li><a href="category-list.html">Chicken</a></li>
+                                            <li><a href="category-list.html">Sliced Deli Meat</a></li>
+                                            <li><a href="category-list.html">Wild Caught Fillets</a></li>
+                                            <li><a href="category-list.html">Crab and Shellfish</a></li>
+                                        </ul>
+                                    </li>
+
+                                </ul>
+                            </li>
+                            <li> <a href="category-grid.html">Guides</a> </li>
+                            <li> <a href="category-masonry.html">Food</a> </li>
+                            <li> <a href="category-big.html">Hotels</a> </li>
+                            <li> <a href="category.html">Review</a> </li>
+                            <li> <a href="category.html">Healthy </a> </li>
+                            <li> <a href="category.html">Lifestyle</a> </li>
+                            <li> <a href="category.html">Blog</a> </li>
+                        </ul>
+                        <!--Mobile menu-->
+                        <ul id="mobile-menu" class="d-block d-lg-none text-muted">
+                            <li class="menu-item-has-children">
+                                <a href="index.html">Home</a>
+                                <ul class="sub-menu text-muted font-small">
+                                    <li><a href="index.html">Home default</a></li>
+                                    <li><a href="home-2.html">Homepage 2</a></li>
+                                    <li><a href="home-3.html">Homepage 3</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children"><a href="#">Pages</a>
+                                <ul class="sub-menu font-small">
+                                    <li><a href="page-about.html">About</a></li>
+                                    <li><a href="page-contact.html">Contact</a></li>
+                                    <li><a href="page-typography.html">Typography</a></li>
+                                    <li><a href="page-register.html">Register</a></li>
+                                    <li><a href="page-login.html">Login</a></li>
+                                    <li><a href="page-search.html">Search</a></li>
+                                    <li><a href="page-author.html">Author</a></li>
+                                    <li><a href="page-404.html">404 page</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children"><a href="#">Category</a>
+                                <ul class="sub-menu font-small">
+                                    <li><a href="category-list.html">List layout</a></li>
+                                    <li><a href="category-grid.html">Grid layout</a></li>
+                                    <li><a href="category-masonry.html">Masonry layout</a></li>
+                                    <li><a href="category-big.html">Big layout</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children"><a href="#">Single post</a>
+                                <ul class="sub-menu font-small">
+                                    <li><a href="single.html">Default</a></li>
+                                    <li><a href="single-2.html">Big image</a></li>
+                                    <li><a href="single-3.html">Left image</a></li>
+                                    <li><a href="single-4.html">With sidebar</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="float-end header-tools text-muted font-small">
+                    <ul class="header-social-network d-inline-block list-inline mr-15">
+                        <li class="list-inline-item"><a class="social-icon fb text-xs-center" target="_blank" href="#"><i class="elegant-icon social_facebook"></i></a></li>
+                        <li class="list-inline-item"><a class="social-icon tw text-xs-center" target="_blank" href="#"><i class="elegant-icon social_twitter "></i></a></li>
+                        <li class="list-inline-item"><a class="social-icon pt text-xs-center" target="_blank" href="#"><i class="elegant-icon social_pinterest "></i></a></li>
+                    </ul>
+                    <div class="off-canvas-toggle-cover d-inline-block">
+                        <div class="off-canvas-toggle hidden d-inline-block" id="off-canvas-toggle">
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style scoped>
-.header {
-  background-color: #42b983;
-  color: white;
-  padding: 20px;
-  text-align: center;
-}
-</style>
